af9d7b15c40b1c6545fc08468c540c406bd2e588
madisonleidner <113846040+madisonleidner@users.noreply.github.com>
2023-03-28 19:02:00 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,10 +1,15 @@
-var video;
-video = document.querySelector('#player1')
-
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
-});
+//Create all variables
+let video = document.querySelector("#player1");
+let play_button = document.querySelector("#play");
+let pause_button = document.querySelector("#pause");
+let slow_down = document.querySelector("#slower");
+let speed_up = document.querySelector("#faster");
+let skip_ahead = document.querySelector("#skip");
+let mute_button = document.querySelector("#mute");
+let volume_slider = document.querySelector("#slider");
+let volume = document.querySelector("#volume");
+let styled = document.querySelector("#vintage");
+let original = document.querySelector("#orig");
 
 //document.querySelector("#play").addEventListener("click", function() {
 //onsole.log("Play Video");
@@ -12,38 +17,41 @@ window.addEventListener("load", function() {
 
 //Page load - Initialize the video element and turn off autoplay and turn off looping.
 window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video.load();
 	video.autoplay = false;
 	video.loop = false;
-	console.log('autoplay is set to ' + video.autoplay);
+	console.log('autoplay is set to ' + video.autoplay)
 	console.log('loop is set to ' + video.loop)
 });
 
 //Play button - Play the video and update the volume information. 
-doccument.querySelector("#play").addEventListener("click", function() {
+play_button.addEventListener("click", function() {
 	video.play()
-	volume = document.querySelector("#slider").value 
-	document.querySelector("volume").innerHTML = volume + '%'
+	volume.innerHTML = video.volume * 100 + '%' 
+	console.log("Play Video");
 });
 
 //Pause botton - pause video
-document.querySelector('#pause').addEventListener("click", function(){
-	video.pause()
+pause_button.addEventListener("click", function(){
+	video.pause();
+	console.log("Pause Video")
 })
 
 //Slow down - Slow the current video speed by 10% each time the button is clicked and log the new speed to the console. 
-document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate *= 0.90
-	console.log("New speed: " + video.playbackRate)
+slow_down.addEventListener("click", function() {
+	video.playbackRate *= 0.90;
+	console.log("New speed: " + video.playbackRate);
 })
 
 //Speed up - Increase the current video speed each time the button is clicked and log the new speed to the console.  Change it by an amount proportional to the slow down. CHECK THIS!!  If you slow down three times and then speed up three times you should be within 5 digits of 100% again.
-document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate *= 1.10
-	console.log("New speed: " + video.playbackRate)
+skip_ahead.addEventListener("click", function() {
+	video.playbackRate *= 1.10;
+	console.log("New speed: " + video.playbackRate);
 })
 
 //Skip ahead - Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther.   Log the current location of the video.
-document.querySelector("#skip").addEventListener("click", function() {
+skip_ahead.addEventListener("click", function() {
 	if (video.currentTime + 10 >= 67.403333){
 		video.currentTime = 0
 		video.play()
@@ -56,30 +64,28 @@ document.querySelector("#skip").addEventListener("click", function() {
 
 //Mute - Mute/unmute the video and update the text in the button.
 document.querySelector('#mute').addEventListener("click", function() {
-	if (video.muted == false) {
-		video.muted = true;
-		document.querySelector("#mute").innerText = 'Unmute'
+	if (video.muted == true) {
+		mute_button.innerHTML = "Mute";
+		video.muted = false;
 	}
 	else{
-		video.muted = false;
-		document.querySelector("#mute").innerText = 'Mute'
+		mute_button.innerHTML = "Unmute";
+		video.muted = true;
 	}
 })
 
 //Volume Slider - Change the volume based on the slider and update the volume information.
-var slider = dociment.getElementById("slider");
-var output = document.getElementById("volume");
-slider.oninput = function() {
-	output.innerHTML = this.value + '%';
-	video.volume = slider.value / 100
-}
+volume_slider.addEventListener("change", function(){
+	video.volume = volume_slider.value / 100;
+	volume.innerHTML = video.volume + 100 + "%"
+})
 
 //Styled - Utilize the existing oldSchool class on the video element
-document.querySelector('#vintage').addEventListener("click", function() {
+styled.addEventListener("click", function() {
 	video.className += " oldSchool"
 })
 
 //Original - Remove the oldSchool class from the video.
-document.querySelector("#orig").addEventListener("click", function() {
+original.addEventListener("click", function() {
 	video.className = "video"
 })
